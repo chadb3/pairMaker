73b58b2fd682c11cfb50d4221aa1e701080fd5fa
Chad Bachman <chadb78999@gmail.com>
2023-02-07 07:27:03 -0600

    Initial Commit

---
+++ pairMaker.py
@@ -0,0 +1,20 @@
+# The purpose of this python app is to detect the correct amount of servers that are in a group.
+# I know that there is a minimum of 2 (so a pair). but there is a need to generate up to the max.
+# to aid in running jobs. 
+# Example the "appServerXY01" and "appServerXY03" come in as unhealthy, but we might need to check all of them in the group. 
+# We know that there is typically an even number of servers in the group. so we generate the text of 01-04 in this instance.
+
+def main():
+	print("Generate")
+	return 0
+	
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
